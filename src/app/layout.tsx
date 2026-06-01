783125a165ad39ca3a1b2b7b2f9ae84be2ec1dd7
Yegar Girsang <yegargirsang@gmail.com>
2026-06-01 15:04:24 +0800

    fix layout and homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,31 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-// 1. Impor komponen Navbar dan Hakcipta dari foldernya
-import Navbar from "../components/Navbar"
-import Hakcipta from "../components/Hakcipta"
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] })
+import Navbar from "../components/Navbar";
+import Hakcipta from "../components/Hakcipta";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SMA Negeri 7 Balikpapan",
   description: "Website Resmi SMA Negeri 7 Balikpapan",
-}
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="id">
       <body className={inter.className}>
-        {/* 1. Pasang Navbar di paling atas */}
         <Navbar />
-        
-           <main>{children}</main>
-        
-        {/* 3. Pasang Hakcipta di paling bawah */}
+
+        <main>{children}</main>
+
         <Hakcipta />
       </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
-import Navbar from "../components/Navbar";
 import Hero from "../sections/Hero";
 import Stats from "../sections/Stats";
 import BeritaHome from "../sections/BeritaHome";
 import Map from "../components/Map";
 import Footer from "../components/Footer";
 
-
 export default function Home() {
   return (
-    <main>
-      <Navbar />
+    <>
       <Hero />
       <Stats />
       <BeritaHome />
       <Map />
       <Footer />
-    </main>
-  )
+    </>
+  );
 }
