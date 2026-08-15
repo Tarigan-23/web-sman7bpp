5a0a3588ca90e3af180bad415390adce2df16173
Yegar Girsang <yegargirsang@gmail.com>
2026-08-15 23:27:13 +0800

    tambah db.js untuk integrasi hostinger

---
+++ db.js
@@ -0,0 +1,8 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;
+
+const db = createClient(supabaseUrl, supabaseKey);
+
+module.exports = db;
