0119793633ca40ee9359645ee5e79c927445ed6f
Yegar Girsang <yegargirsang@gmail.com>
2026-08-20 21:40:56 +0800

    fix: tambahkan @supabase/ssr ke dependencies agar middleware tidak error

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cogsouobijaqdbglesol.supabase.co",
-        port: "",
-        pathname: "/storage/v1/object/public/**",
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        // Mencakup semua rute/halaman di web utama
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://virtualtour.sman7-bpp.sch.id" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
+
 };
 
 export default nextConfig;
